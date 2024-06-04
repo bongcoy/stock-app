8d79f73446c66745647b1d7adfaadf4c676a36ce
bongcoy <mpascaldewantara@gmail.com>
2024-06-04 22:21:13 +0700

    add tailwind options for nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/ui"]
-})
+  devtools: {enabled: true},
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/ui"],
+  tailwindcss: {
+    cssPath: ["./assets/css/tailwind.css", {injectPosition: "first"}],
+    configPath: "tailwind.config",
+    exposeConfig: {
+      level: 2,
+    },
+    config: {},
+    viewer: true,
+  },
+});
